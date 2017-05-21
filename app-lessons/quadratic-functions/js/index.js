3260e50548e00dd02f345de9c38e28e2e2fb7eaf
p <sisuser@SISAdmins-MacBook-Pro.local>
2017-05-21 16:18:11 -0700

    move.js sample

--- app-lessons/quadratic-functions/js/index.js
+++ app-lessons/quadratic-functions/js/index.js
@@ -18,18 +18,32 @@
 						.add('margin-bottom', 75)
 						.duration('1s')
 						.end(function(){
-							move('.co-b')
-							.set('color','white')
-							.add('margin-left', 350)
-							.add('margin-bottom', 75)
-							.duration('1s')
-							.end(function(){
-								move('.co-c')
+								move('.co-b')
 								.set('color','white')
-								.add('margin-left', 450)
+								.add('margin-left', 350)
 								.add('margin-bottom', 75)
 								.duration('1s')
-								.end();
+								.end(function(){
+										move('.co-c')
+										.set('color','white')
+										.add('margin-left', 450)
+										.add('margin-bottom', 75)
+										.duration('1s')
+										.end(function(){
+												move('.plus1')
+												.set('color','white')
+												.add('margin-left', 305)
+												.add('margin-bottom', 75)
+												.duration('1s')
+												.end(function(){
+														move('.plus2')
+														.set('color','white')
+														.add('margin-left', 405)
+														.add('margin-bottom', 75)
+														.duration('1s')
+														.end();
+									});
+								});
 							});
 						});
 				});
