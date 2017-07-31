4c7337d0256f241ee3989707f263cc69b6cb6c2f
p <sisuser@SISAdmins-MacBook-Pro.local>
2017-07-30 19:38:16 -0700

    latest

--- app-lessons/quadratic-functions/js/graphs-js/app.js
+++ app-lessons/quadratic-functions/js/graphs-js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $(document).foundation();
     var loader = $('body').loadingIndicator({
                     useImage: false,
                 }).data("loadingIndicator");
@@ -6,30 +7,48 @@ $(document).ready(function () {
         loader.hide();
 
         var plot = "#plot";
+        var $eqInput = $(plot).parent().find(".eq");
 
         //Draw function-plot
-    	slider.draw(plot);
+    	slider.draw(plot, $eqInput);
 
-        //Assign co-efficients
+        //TODO: CREATE MAPPING OBJECT
+        //#sliderhandle1 (create class)
+        //#sliderhandle2 (create class)
+        //#sliderhandle3 (create class)
+        //#eq (create class)
+        //number of sliders
+        /*
+        var graph = {
+            "num-of-sliders" : slider-num,
+            "start-num" : start-num,
+            "end-num" : end-num,
+            "increment" : increment,
+            "eq" : equation
+        };
+        */
+
+        /*
         slider.currentCoefficients = {
           "x2": "1",
           "x": "1",
           "c": "1"
         };
+        */
+
+        //Assign co-efficients
+        slider.currentCoefficients = {
+          "x2": "1",
+          "x" : "1",
+          "c" : "1"
+        };
 
     	// Attach Event listeners
-        $('.slider1').on('moved.zf.slider', slider.slider1);
+        $('.slider1').on('moved.zf.slider', slider.slider1(plot,$eqInput));
         $('.slider2').on('moved.zf.slider', slider.slider2);
         $('.slider3').on('moved.zf.slider', slider.slider3);
 
     });
-
-    var options = {
-            target: '#graph',
-            data: [{
-                fn: 'ax^2'
-            }]
-        };
 });
 
 var slider = {
@@ -37,7 +56,7 @@ var slider = {
     currentCoefficients: {},
 
     //plot: is the element id of the graph to draw
-    draw: function(plot) {
+    draw: function(plot, eq) {
         try {
             functionPlot({
                 target: plot,
@@ -54,7 +73,7 @@ var slider = {
                     }
                 },
                 data: [{
-                    fn: document.getElementById('eq').value,
+                    fn: eq.val(),
                     sampler: 'builtIn',  // this will make function-plot use the evaluator of math.js
                     graphType: 'polyline'
                 }]
@@ -66,75 +85,82 @@ var slider = {
         }
     },
 
-    slider1: function() {
-        var valueOfSlider1 = $(this).children('#slider-handle1').attr('aria-valuenow');
-        console.log('valueOfSlider1 = ' + valueOfSlider1);
+    slider1: function(plot, $eq) {
+        return function (event) {
+            console.log(event.target);
+            var valueOfSlider1 = $(event.target).children('.slider-handle1').attr('aria-valuenow');
+            console.log('valueOfSlider1 = ' + valueOfSlider1);
 
-        currentCoefficients.x2 = valueOfSlider1;
+            slider.currentCoefficients.x2 = valueOfSlider1;
 
-        var eqVar = $('#eq').val();
-        console.log('the value of #eq is: ' + eqVar);
-        console.log('The value of currentCoefficients is: ' + currentCoefficients);
+            var eqVar = $eq.val();
+            console.log('the value of #eq is: ' + eqVar);
+            console.log('The value of currentCoefficients is: ', slider.currentCoefficients);
 
-        var newEquation = currentCoefficients.x2 + 'x^2 + ' + currentCoefficients.x + 'x + ' + currentCoefficients.c;
-        console.log('newEquation is: ' + newEquation);
+            var newEquation = slider.currentCoefficients.x2 + 'x^2 + ' + slider.currentCoefficients.x + 'x + ' + slider.currentCoefficients.c;
+            console.log('newEquation is: ' + newEquation);
 
-        $('#eq').text(newEquation).attr(newEquation);
+            $eq.text(newEquation).attr(newEquation);
 
-        if (valueOfSlider1 > 1){
-            $('#eq').val();
-        } else {
-            newEquation = 'x^2 + ' + currentCoefficients.x + 'x + ' + currentCoefficients.c;
+            if (valueOfSlider1 > 1){
+                $eq.val();
+            } else {
+                newEquation = 'x^2 + ' + slider.currentCoefficients.x + 'x + ' + slider.currentCoefficients.c;
+            }
+
+            var valueOfEq = $eq.val();
+            console.log(valueOfEq);
+
+            setTimeout(function(){
+                $eq.val(newEquation);
+                slider.draw(plot, $eq);
+            }, 1);
         }
-
-        var valueOfEq = $("#eq").val();
-        console.log(valueOfEq);
-
-        setTimeout(function(){
-            $('#eq').val(newEquation);
-            draw();
-        }, 1);
     },
 
     slider2: function() {
-        var valueOfSlider2 = $(this).children('#slider-handle2').attr('aria-valuenow');
-        console.log('valueOfSlider2 = ' + valueOfSlider2);
+        return function (event) {
+            var valueOfSlider2 = $(this).children('#slider-handle2').attr('aria-valuenow');
+            console.log('valueOfSlider2 = ' + valueOfSlider2);
 
-        var newEquation2 = 'x^2 + ' + valueOfSlider2 + 'x + 1';
+            var newEquation2 = 'x^2 + ' + valueOfSlider2 + 'x + 1';
 
-        currentCoefficients.x = valueOfSlider2;
+            currentCoefficients.x = valueOfSlider2;
 
-        var eqVar2 = $('#eq').val();
-        console.log('the value of #eq is: ' + eqVar2);
-        console.log('The value of currentCoefficients is: ' + currentCoefficients);
+            var eqVar2 = $('#eq').val();
+            console.log('the value of #eq is: ' + eqVar2);
+            console.log('The value of currentCoefficients is: ' + currentCoefficients);
 
-        var newEquation2 = currentCoefficients.x2 + 'x^2 + ' + currentCoefficients.x + 'x + ' + currentCoefficients.c;
-        console.log('newEquation is: ' + newEquation2);
+            var newEquation2 = currentCoefficients.x2 + 'x^2 + ' + currentCoefficients.x + 'x + ' + currentCoefficients.c;
+            console.log('newEquation is: ' + newEquation2);
 
-        $('#eq').text(newEquation2).attr(newEquation2);
+            $('#eq').text(newEquation2).attr(newEquation2);
 
-        if (valueOfSlider2 > 1){
-            $('#eq').val();
-        } else {
-            newEquation2 = currentCoefficients.x2 + 'x^2 + ' + 'x + ' + currentCoefficients.c;
+            if (valueOfSlider2 > 1){
+                $('#eq').val();
+            } else {
+                newEquation2 = currentCoefficients.x2 + 'x^2 + ' + 'x + ' + currentCoefficients.c;
+            }
+
+            setTimeout(function(){
+                $('#eq').val(newEquation2);
+                draw();
+            }, 1);
         }
-
-        setTimeout(function(){
-            $('#eq').val(newEquation2);
-            draw();
-        }, 1);
     },
 
     slider3: function() {
-        var valueOfSlider3 = $(this).children('#slider-handle3').attr('aria-valuenow');
-        console.log('valueOfSlider3 = ' + valueOfSlider3);
-        var newEquation3 = currentCoefficients.x2 + 'x^2 + ' + currentCoefficients.x + 'x + ' + valueOfSlider3 + ' ' ;
+        return function (event) {
+            var valueOfSlider3 = $(this).children('#slider-handle3').attr('aria-valuenow');
+            console.log('valueOfSlider3 = ' + valueOfSlider3);
+            var newEquation3 = currentCoefficients.x2 + 'x^2 + ' + currentCoefficients.x + 'x + ' + valueOfSlider3 + ' ' ;
 
-        currentCoefficients.c = valueOfSlider3;
+            currentCoefficients.c = valueOfSlider3;
 
-        setTimeout(function(){
-            $('#eq').val(newEquation3);
-            draw();
-        }, 1);
+            setTimeout(function(){
+                $('#eq').val(newEquation3);
+                draw();
+            }, 1);
+        }
     }
 };
